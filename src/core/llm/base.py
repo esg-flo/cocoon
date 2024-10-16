f7afd3fa09ca942369a5e31bd9b0583379b484c4
Sagar Thacker <168448988+sagar-thacker@users.noreply.github.com>
2024-10-16 14:20:06 -0400

    Create cocoon package and refactored imports, logging

--- src/core/llm/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class LLM(ABC):
-    @abstractmethod
-    def invoke(self):
-        pass
--- src/core/messages/human.py
+++
@@ -1,8 +0,0 @@
-from core.messages.base import BaseMessage
-
-
-class HumanMessage(BaseMessage):
-    type: str = "human"
-
-    def __init__(self, content: str):
-        super().__init__(content=content)
