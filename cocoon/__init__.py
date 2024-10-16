9ccb1482ec480a4a54293e2435603152c08e1021
Sagar Thacker <168448988+sagar-thacker@users.noreply.github.com>
2024-10-16 14:24:08 -0400

    Removed redundant comments

--- cocoon/__init__.py
+++ cocoon/__init__.py
@@ -1,9 +1,3 @@
-# import sys
-# from pathlib import Path
-
-# ROOT_PATH = Path(__file__).resolve().parent
-# sys.path.insert(0, ROOT_PATH)
-
 from .cluster_service import compute_cluster
 from .embedding_service import create_embeddings, initialize_faiss_index_from_embeddings
 from .output_service import generate_output
