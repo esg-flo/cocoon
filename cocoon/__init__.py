1f7f6bd6baa43c815077a0b08ce31168b609aa16
Sagar Thacker <168448988+sagar-thacker@users.noreply.github.com>
2024-10-18 08:33:45 -0400

    Updated to run similarity search on all rows

--- cocoon/__init__.py
+++ cocoon/__init__.py
@@ -1,4 +1,3 @@
-from .cluster_service import compute_cluster
 from .embedding_service import create_embeddings, initialize_faiss_index_from_embeddings
 from .output_service import generate_output
 from .search_service import find_entity_relation_matches_and_cluster
@@ -6,7 +5,6 @@ from .search_service import find_entity_relation_matches_and_cluster
 __all__ = [
     "core",
     "utils",
-    "compute_cluster",
     "create_embeddings",
     "initialize_faiss_index_from_embeddings",
     "generate_output",
