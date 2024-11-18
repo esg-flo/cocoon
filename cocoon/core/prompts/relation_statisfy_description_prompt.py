feb790bc4ab64c05154f54705d3184ca0fcd116d
Sagar Thacker <168448988+sagar-thacker@users.noreply.github.com>
2024-11-19 03:28:07 +0530

    Save partial results & handle output format better

--- cocoon/core/prompts/relation_statisfy_description_prompt.py
+++ cocoon/core/prompts/relation_statisfy_description_prompt.py
@@ -4,8 +4,7 @@ Below are entities:
 Below are the description:
 {}
 
-Now, find entities that satisfy the description. Provide your answer as json:
-```json
+Now, find entities that satisfy the description. Provide your answer as RFC8259 compliant JSON response following the output format without deviation.:```\n
 {{
     "reasoning": "The entities are about ...",
     "indices": [...] (list of index numbers, could be empty)
